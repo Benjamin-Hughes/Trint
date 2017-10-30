3b47eec543abb486fdb78495cb343251a56ef5a8
Ben Hughes <benjamin-hughes@users.noreply.github.com>
2017-10-30 12:53:57 +0000

    add test for updateDate to ItemSpec

--- spec/itemSpec.js
+++ spec/itemSpec.js
@@ -9,7 +9,15 @@ describe('item', function() {
     expect(item.site).not.toBe(null);
   })
 
-  it('should store the current date upon creation', function() {
+  it('should store a date upon creation', function() {
     expect(this.date).not.toBe(null);
+  })
+
+  describe('updateDate', function() {
+
+    it('should update the date to the present date', function() {
+      item.updateDate();
+      expect(item.date).not.toMatch("December 17, 1995 03:24:00");
+    })
   })
 })
